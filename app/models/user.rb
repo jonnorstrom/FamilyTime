f58822f4fd5e25651ef14c2674871c61fe56c92a
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-06-16 13:42:10 -0500

    trying to get it to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :invitations
   has_many :vacations, through: :invitations
-  has_secure_password
+  # attr_accessible :email, :password, :password_confirmation
+  # validates_presence_of :password, :on => :create
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
 end
