c9e8542e733f7954f131dc71d27276edb9290fb2
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-06-16 17:05:31 -0500

    get different views to render

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,5 +1,10 @@
 class SplashController < ApplicationController
   def index
-    session[:current_user_id] = 1
+    # session[:current_user_id] = 2
+    if !current_user
+      render :index
+    else
+      redirect_to vacations_path
+    end
   end
 end
