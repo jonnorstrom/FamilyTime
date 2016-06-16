3a91d60216b98a610139cc03fd7f060943ed9086
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-06-16 16:40:51 -0500

    users can change their password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :invitations
   has_many :vacations, through: :invitations
+
+  has_secure_password
   # attr_accessible :email, :password, :password_confirmation
   # validates_presence_of :password, :on => :create
 
@@ -13,5 +15,10 @@ class User < ActiveRecord::Base
   def password=(new_password)
     @password = ::BCrypt::Password.create(new_password)
     self.password_digest = @password
+  end
+
+  def update_password(password)
+    self.update(password_digest: ::BCrypt::Password.create(password))
+    self.save
   end
 end
