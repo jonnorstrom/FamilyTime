258a8bb68f1361434981e18cd8d4c33c09be97c0
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-06-21 17:47:53 -0500

    users can sign up for meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,11 @@ class Meal < ActiveRecord::Base
   belongs_to :vacation
   belongs_to :user
 
-  def owner
-    User.find(self.user_id).full_name
+  def statement
+    if self.user_id
+      "#{User.find(self.user_id).full_name} has the meal"
+    else
+      "Unaccounted for - only Admins can sign up"
+    end
   end
 end
