580c874a9af05c0021cdb6ec0fa30f54c4000d3a
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-06-21 14:26:11 -0500

    adding vacation and user model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create(first_name: "Jon", last_name: "Norstrom", email: "email@email.com", password: "password", is_admin: true)
+  end
+
+  test "password is hashed by BCrypt" do
+    assert(@user.password_digest.length > 30)
+  end
+
+  test "password is hashed with BCrypt salt" do
+    assert_equal(@user.password_digest[0..6], "$2a$10$")
+  end
+
+  test "password authentication works" do
+    assert(@user.authenticate("password"))
+  end
+
+  test "password authentication fails with wrong password" do
+    assert_not(@user.authenticate("not password"))
+  end
+
+  test "password can be changed" do
+    @user.update_password("new password")
+    assert(@user.authenticate("new password") && !@user.authenticate("password"))
+  end
+
+  test "full name returns users first and last name" do
+    assert_equal(@user.full_name, "Jon Norstrom")
+  end
 end
--- test/models/vacation_test.rb
+++ test/models/vacation_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class VacationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @vacation = Vacation.create(name: "Test Vacation")
+  end
+
+  test "makes new vacation topics on create" do
+    assert_equal(@vacation.topics.count, 3)
+  end
+
+  test "doesn't make new topics on any update" do
+    @vacation.update(name: "New Name", start: Date.today)
+    assert_equal(@vacation.topics.count, 3)
+  end
+
+  test "doesn't make meals when dates aren't present" do
+    assert_empty(@vacation.meals)
+  end
+
+  class VacationTest <ActiveSupport::TestCase
+    setup do
+      @vacation = Vacation.create(name: "Test Vacation")
+      @vacation.update(start: Date.new(2016,4,12), end: Date.new(2016,4,20))
+    end
+
+    test "makes meals when dates are present" do
+      assert_not_empty(@vacation.meals)
+    end
+
+    test "makes the corrent amount of meals" do
+      assert(@vacation.meals.first.day = Date.new(2016,4,12) && @vacation.meals.last.day = Date.new(2016,4,20))
+    end
+
+    test "it changes meals when the vacation dates change" do
+      @vacation.update(start: Date.new(2016,4,24), end: Date.new(2016,5,9))
+      assert(@vacation.meals.first.day = Date.new(2016,4,12) && @vacation.meals.last.day = Date.new(2016,4,20))
+    end
+  end
 end
