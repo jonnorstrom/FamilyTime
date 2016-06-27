0b6c6c3956a74aba9b06626c00fc6ec66521c73b
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-06-27 18:44:53 -0500

    gs

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,23 +1,17 @@
 class MealsController < ApplicationController
-  before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   def update
     @meal = Meal.find(params[:meal][:meal_id])
     @meal.update(user_id: params[:meal][:user_id])
 
     if @meal.save
-      redirect_to([@topic.vacation, @topic], notice: 'Meal was successfully updated.')
+      redirect_to(@vacation, notice: 'Meal was successfully updated.')
     else
       render action: 'new'
     end
   end
 
   private
-
-    def set_topic
-      @topic = @vacation.topics.find(params[:id])
-    end
-
     def meal_params
       params.require(:meal).permit(:day, :user_id)
     end
